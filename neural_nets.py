958ab6a4b3cacb9c6f983993130aae7d7af7b26a
willaccc <chengyww@gmail.com>
2016-07-11 12:45:02 -0700

    Update neural_nets.py

--- neural_nets.py
+++ neural_nets.py
@@ -2,6 +2,10 @@
 """
 Created on Sun Jul 10 23:08:21 2016
 
+Predicting number of child injuries 
+in the age category of less than 1 year old 
+using census tract data regarding children age/sex data. 
+
 @author: Willa Cheng
 """
 # Import data
@@ -33,20 +37,24 @@ from NN import *
 NN = Neural_Network()
 T = trainer(NN)
 
+# Cross Validation with K-Fold
 num_folds = 7
 subset_size = len(y1)/num_folds
-yHat
+
 for i in range(num_folds):
     X_testing_this_round = X[i*subset_size:,:][:subset_size,:]
     X_training_this_round = np.append(X[:i*subset_size,:] , X[(i+1)*subset_size:,:],axis = 0)
     y1_testing_this_round = y1[i*subset_size:][:subset_size]
     y1_training_this_round = np.append(y1[:i*subset_size] , y1[(i+1)*subset_size:],axis = 0)
+    # Train the model
     T.train(X_training_this_round, y1_training_this_round)
     NN.costFunctionPrime(X_training_this_round,y1_training_this_round)
+    # predicting value
     yHat = NN.forward(X_testing_this_round)
     y1_testing_this_round = y1_testing_this_round.ravel()
     yHat = yHat.ravel()
+    # Evaluate with Mean Squared Error
     MSE = sum((y1_testing_this_round - yHat)**2)/subset_size
     print(MSE)
-    # evaluate against testing_this_round
-    # save accuracy
+
+
